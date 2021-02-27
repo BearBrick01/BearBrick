67edc4d2cfcfd7a1025ac59e39f05ded2f7cbfba
tncht <chathanya02@gmail.com>
2021-02-28 06:39:23 +0700

    PHP 1

--- register.php
+++ register.php
@@ -1,9 +1,42 @@
+<?php
+session_start();
+
+require_once "connect.php";
+
+
+    if(isset($_SESSION["Username"])){  
+        header("Location:entry.php");  
+    }
+
+    if (isset($_POST['Register']) ){
+      $mysqli = new mysqli("localhost", "root", "", "bearbrick");
+     
+      if(empty($_POST["Fname"]) && empty($_POST["Lname"]) && empty($_POST["Email"]) && empty($_POST["Username"]) && empty($_POST["Username"]) )
+      {  
+        echo '<script>alert("Fields are required")</script>';  
+
+      }else{
+        
+        $Fname = $_POST['Fname'];
+        $Lname = $_POST['Lname'];
+        $Email = $_POST['Email'];
+        $Password = $_POST['Password'];
+        $Username = $_POST['Username'];
+        $Password = md5($Password); 
+        
+        $query = "INSERT INTO costomer (idcostomer,Fname,Lname,Email,Password,Username)
+                  VALUES(null, '$Fname', '$Lname', '$Email', '$Password' ,'$Username')";  
+         mysqli_query($conn, $query);
+      }
+    }
+    
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>BearBrick | Log in</title>
+  <title>BearBrick | Register</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -24,11 +57,11 @@
     <div class="card-body register-card-body">
       <p class="login-box-msg">Register a new membership</p>
 
-      <form action="../../index.html" method="post">
+      <form method="post">
         <div class="form-row">
             <div class="col"> 
                 <div class="input-group mb-3">
-                    <input type="text" class="form-control" placeholder="Fristname">
+                    <input type="text" class="form-control" id="Fname" name="Fname" placeholder="Fristname">
                     <div class="input-group-append">
                     <div class="input-group-text">
                         <span class="fas fa-user"></span>
@@ -38,7 +71,7 @@
             </div>
             <div class="col"> 
                 <div class="input-group mb-3">
-                    <input type="text" class="form-control" placeholder="Lastname">
+                    <input type="text" class="form-control" mid="Lname" name="Lname"  placeholder="Lastname">
                     <div class="input-group-append">
                     <div class="input-group-text">
                         <span class="fas fa-user"></span>
@@ -48,7 +81,7 @@
             </div>
         </div>
         <div class="input-group mb-3">
-          <input type="email" class="form-control" placeholder="Email">
+          <input type="email" class="form-control" id="Email" name="Email"  placeholder="Email">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-envelope"></span>
@@ -56,15 +89,15 @@
           </div>
         </div>
         <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Password">
+          <input type="text" class="form-control" id="Username" name="Username" placeholder="Username">
           <div class="input-group-append">
             <div class="input-group-text">
-              <span class="fas fa-lock"></span>
+              <span class="fas fa-user"></span>
             </div>
           </div>
         </div>
         <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Retype password">
+          <input type="password" class="form-control" id="Password" name="Password"  placeholder="Password">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-lock"></span>
@@ -75,10 +108,10 @@
           <!-- /.col -->
           <div class="row">
             <div class="col-md-4 offset-md-4">
-            <button type="submit" class="btn btn-primary btn-block">Register</button>
+            <button type="submit" class="btn btn-primary btn-block" id="Register" name="Register">Register</button>
             </div>
             <div class="col-7"></div>
-            <div class="col-5"><a href="login.html" class="text-center"><small> I already have a membership </small></a></div> 
+            <div class="col-5"><a href="login.php" class="text-center"><small> I already have a membership </small></a></div> 
          </div>
           <!-- /.col -->
         </div>
@@ -98,3 +131,4 @@
 <script src="../../dist/js/adminlte.min.js"></script>
 </body>
 </html>
+
